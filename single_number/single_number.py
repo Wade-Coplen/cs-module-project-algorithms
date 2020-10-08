80a3cc2ff26f251119e7c31f64cd09046720e5f2
Wade-Coplen <wadecoplen@gmail.com>
2020-10-08 15:07:46 -0700

    Still having issues with single number. Moving on and will circle back

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -8,28 +8,38 @@ U-
             i[0] i[1]
 # is this list sorted?
 # what if it is unsorted?
-# We are going to have to 
-# we need to look at each index. Then compare that index to the previous index and next index..i[0] <--> i[1]<--> i[2]
+# We are going to have to do if we cannot sort the list. iterate and pop and append?
+# if sorted, we need to look at each the first index. Then compare that index to the previous index and next index..i[0] <--> i[1]<--> i[2]
 # if no previous to compare, move to next index. 
 # if current_index is == previous_index or current_index is == next_index, it is not the single integer we are looking for. Next
 # if current_index is != prev_index nor is it equal to the next_index, return current_index
-# so this list CANNOT be sorted as it runs of a random number generator. Each run has a new set of numbers. 
+# range(0, len(a), 2)
 '''
 def single_number(arr):
     # Your code here
-    sorted_array = list(sorted(arr))
-    print(sorted_array)
-    for i in len(sorted_array) - 1:
-        current_index = sorted_array[i]
-        next_index = current_index + 1
-        previous_index = current_index - 1
-        if current_index -1 == current_index or current_index == next_index:
-            return None
-        if current_index == next_index or previous_index:
-            return current_index
-        else:
-            if i != next_index or previous_index:
-               return current_index
+    sorted_list = list(sorted(arr))
+    #print(sorted_list)
+    new_list = []
+    for i in range(0, len(sorted_list) - 1):
+        for j in sorted_list:
+            if sorted_list[i] == sorted_list[j]:
+                continue
+            else:
+                new_list.append(j)
+    print(new_list)
+       
+   
+            
+           
+    
+        
+        
+    
+
+    
+
+        
+
 
 
 
