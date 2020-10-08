4bc6c74c6593b146de86cd8f9ea2562ffb56d9a0
Wade-Coplen <wadecoplen@gmail.com>
2020-10-08 15:55:37 -0700

    cant figure out this one. Moving on

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,8 +1,30 @@
 '''
 Input: a List of integers
 Returns: a List of integers
+--------------------------
+Understanding the Problem:
+Could be positive or negative integer
+It says the LEFT side of the array, which infers there is a RIGHT side, which
+means there is a MIDDLE
+
+LEFT - anything that is NOT ZERO, which is anything but zero
+MIDDLE --> start + end // 2
+RIGHT - has to be 0
+
+Possibly a binary searching left -1 and right -1
+
 '''
 def moving_zeroes(arr):
+    # Find the middle
+    # iterate over the list and divide length by floor 2
+    # have to find a middle
+    for i in range(0, len(arr) -1):
+        middle = (len(arr) -1) // 2
+        #print(middle)
+     
+        if arr[middle] == 0:
+            return arr[middle + 1]
+
     # Your code here
 
     pass
