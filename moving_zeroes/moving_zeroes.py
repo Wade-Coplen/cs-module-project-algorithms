5c65a6559ace65d55d24e3d6a28bc28b643bcd43
Wade-Coplen <wadecoplen@gmail.com>
2020-10-09 21:41:14 -0700

    I achieved what they asked, but in a different way so the tests wont pass...lol

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -13,21 +13,45 @@ RIGHT - has to be 0
 
 Possibly a binary searching left -1 and right -1
 
+1. Find the middle
+[0, 3, 1, 0, -2]
+       ^
+        middle = (len(arr) -1) // 2
+2. Split up into 3 lists
+    a. [0, 3]
+    b. [1]
+    c. [0, 2]
+-or-
+ iterate through the list. if anything is bigger than 0, append
+ to its own list. 
+ 3. Should have two lists. [0,0] and [3, 1, 2]
+ 4. join two lists
 '''
 def moving_zeroes(arr):
     # Find the middle
     # iterate over the list and divide length by floor 2
     # have to find a middle
-    for i in range(0, len(arr) -1):
-        middle = (len(arr) -1) // 2
-        #print(middle)
-     
-        if arr[middle] == 0:
-            return arr[middle + 1]
+    before = []
+    middle_index = (len(arr) -1) // 2
+    #print(middle_index)
+    middle = []
+    after = []
+    for i in arr:
+        #print(i)
+        if i == 0:
+            middle.append(i)
+            #print(middle)
+        elif i is not 0:
+            before.append(i)
+           # print(before)
+    for x in before:
+        middle.append(x)
+    print(middle)
+            
 
     # Your code here
 
-    pass
+
 
 
 if __name__ == '__main__':
